04d1087b4a1296030acfe1e08ef9f5a1a3afb7b4
ramirez915 <jose.angelramirez915@gmail.com>
2019-04-17 12:22:15 -0500

    adding client file

---
+++ Client.java
@@ -0,0 +1,99 @@
+package sample;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.net.Socket;
+
+public class Client implements Runnable, Serializable{
+    private String Name;
+    private String played;
+    private int numPoints;
+    private Boolean playAgain;
+    private transient Socket socket;
+    transient ObservableList<String> Played;
+    transient ObservableList<String> points;
+
+    private ObjectOutputStream cliOutput;
+    private ObjectInputStream cliInput;
+
+
+
+    //constructor
+    public Client(String name, String ip, int port) throws IOException {
+        this.Name = name;
+        this.played = " ";
+        this.numPoints = 0;
+        this.playAgain = false;
+        this.socket = new Socket(ip, port);
+        this.Played = FXCollections.observableArrayList();
+        this.points = FXCollections.observableArrayList();
+
+        // added so that each Client obj has its own input and output stream
+        this.cliOutput = new ObjectOutputStream(this.socket.getOutputStream());          // input and output streams
+        this.cliInput = new ObjectInputStream(this.socket.getInputStream());
+        socket.setTcpNoDelay(true);
+    }
+
+    public Client(Client c1) throws IOException{
+        this.Name = c1.Name;
+        this.played = c1.played;
+        this.numPoints = c1.numPoints;
+        this.playAgain = c1.playAgain;
+        this.socket = c1.socket;
+        this.Played = c1.Played;
+        this.points = c1.points;
+
+        // added so that each Client obj has its own input and output stream
+        this.cliOutput = c1.cliOutput;
+        this.cliInput = c1.cliInput;
+        socket.setTcpNoDelay(true);
+    }
+
+    // what a client gets from the server
+    public synchronized void run(){
+        try {
+            while (true) {
+                Serializable data = (Serializable) cliInput.readObject();
+                System.out.println("from server"+data);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //setter functions
+    public void setPlayed(String x){
+        this.played = x;
+    }
+    public void setNumPoints(int x){
+        this.numPoints = x;
+    }
+    public void setPlayAgain(Boolean x){
+        this.playAgain = x;
+    }
+
+    //getter functions
+    public String getName(){
+        return this.Name;
+    }
+    public String getPlayed(){
+        return this.played;
+    }
+    public int getNumPoints(){
+        return this.numPoints;
+    }
+    public Boolean getPlayAgain(){
+        return this.playAgain;
+    }
+    public Socket getSocket(){
+        return this.socket;
+    }
+
+    public ObjectOutputStream getCliObjOut(){ return this.cliOutput;}
+    public ObjectInputStream getCliInput(){ return this.cliInput;}
+}
